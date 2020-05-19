406827edb0bb5fef6c6c40132dee3493e021b812
Artem Kolpakov <41115136+akolpakov1985@users.noreply.github.com>
2020-05-19 09:08:14 +0200

    added search4vowels

---
+++ search4vowels.py
@@ -0,0 +1,8 @@
+def search4vowels():
+    vowels = {'a', 'e', 'i', 'o', 'u'}
+    word = input("Provide a word to search for vowels: ")
+    i = vowels.intersection(set(word))
+    for vowels in i:
+        print(vowels)
+
+search4vowels()
